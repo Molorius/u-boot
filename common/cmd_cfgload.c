5b941833675505116a128469818a1d8db06db5d4
Blake Felt <blake.w.felt@gmail.com>
2019-03-06 22:23:54 -0700

    Added iniload command, which allows running an additional boot script from any partition.

--- common/cmd_cfgload.c
+++ common/cmd_cfgload.c
@@ -40,30 +40,28 @@ static int valid_command(const char* p)
 
 /* Read boot.ini from FAT partition
  */
-static char* read_cfgload(void)
+static char* read_iniload(char* interface, char* dev, char* addr, char* filename)
 {
 	char cmd[128];
 	unsigned long filesize;
 	char *p;
 
-	p = (char *)simple_strtoul(getenv("loadaddr"), NULL, 16);
-	if (NULL == p)
-		p = (char *)CONFIG_SYS_LOAD_ADDR;
-
+	p = addr;
+	
 	setenv("filesize", "0");
 
-	sprintf(cmd, "fatload mmc 0:1 0x%p boot.ini", (void *)p);
+	sprintf(cmd, "fatload %s %s 0x%p %s", interface, dev, (void *)p, filename);
 	run_command(cmd, 0);
 
 	filesize = getenv_ulong("filesize", 16, 0);
 	if (0 == filesize) {
-		printf("cfgload: no boot.ini or empty file\n");
+		printf("iniload: no %s or empty file on %s %s\n", filename, interface, dev);
 		return NULL;
 	}
 
 	if (filesize > SZ_BOOTINI) {
-		printf("boot.ini: 'boot.ini' exceeds %d, size=%ld\n",
-				SZ_BOOTINI, filesize);
+		printf("iniload: %s exceeds %d, size=%ld\n",
+				filename, SZ_BOOTINI, filesize);
 		return NULL;
     }
 
@@ -85,31 +83,32 @@ static char* read_cfgload(void)
 			return memcpy(malloc(filesize), p, filesize);
 	}
 
-	printf("cfgload: MAGIC NAME, %s, is not found!!\n", BOOTINI_MAGIC);
+	printf("iniload: MAGIC NAME, " BOOTINI_MAGIC ", is not found!!\n");
 
 	return NULL;
 }
 
 extern int is_hdmimode_valid(const char *);
 
-static int do_load_cfgload(cmd_tbl_t *cmdtp, int flag, int argc,
-		char *const argv[])
+static int load_iniload(char* interface, char* dev, char* addr, char* filename)
 {
 	char *p;
 	char *cmd;
 
-	p = read_cfgload();
-	if (NULL == p)
+	p = read_iniload(interface, dev, addr, filename);
+	if (NULL == p) {
+		printf("iniload: problem reading specified file\n");
 		return 0;
+	}
 
-	printf("cfgload: applying boot.ini...\n");
+	//printf("cfgload: applying boot.ini...\n");
 
 	while (p) {
 		cmd = strsep(&p, "\n");
 		if (!valid_command(cmd))
 			continue;
 
-		printf("cfgload: %s\n", cmd);
+		printf("iniload: %s\n", cmd);
 
 #ifndef CONFIG_SYS_HUSH_PARSER
 		run_command(cmd, 0);
@@ -118,13 +117,64 @@ static int do_load_cfgload(cmd_tbl_t *cmdtp, int flag, int argc,
 				| FLAG_EXIT_FROM_LOOP);
 #endif
 		/* Check the hdmimode validation */
+		/*
 		if (strstr(cmd, "hdmimode")) {
 			if (!is_hdmimode_valid(getenv("hdmimode")))
 				setenv("hdmimode", "1080p60hz");
 		}
+		*/
 	}
 
-	return 0;
+	return 0;	
+}
+
+static int do_load_cfgload(cmd_tbl_t *cmdtp, int flag, int argc,
+		char *const argv[])
+{
+	char *p;
+	//char *cmd;
+
+	p = (char *)simple_strtoul(getenv("loadaddr"), NULL, 16);
+	if (NULL == p)
+		p = (char *)CONFIG_SYS_LOAD_ADDR;
+	
+	printf("cfgload: applying boot.ini...\n");
+	return load_iniload("mmc", "0:1", p, "boot.ini");
+
+}
+
+static int do_load_iniload(cmd_tbl_t *cmdtp, int flag, int argc,
+		char *const argv[])
+{
+	char *interface, *dev, *addr, *filename;
+
+	// put default arguments, if necessary
+	if(argc<2)
+		interface = "mmc";
+	else
+		interface = argv[1];
+	
+	if(argc<3)
+		dev = "0:1";
+	else
+		dev = argv[2];
+	
+	if(argc<4)
+		filename = "boot.ini";
+	else
+		filename = argv[3];
+
+	if(argc<5) {
+		addr = (char *)simple_strtoul(getenv("loadaddr"), NULL, 16);
+		if(addr == NULL) {
+			addr = (char *)CONFIG_SYS_LOAD_ADDR;
+		}
+	}
+	else
+		addr = argv[4];
+	
+	// load file
+	return load_iniload(interface, dev, addr, filename);
 }
 
 U_BOOT_CMD(
@@ -132,6 +182,13 @@ U_BOOT_CMD(
 		"read 'boot.ini' from FAT partiton",
 		"\n"
 		"    - read boot.ini from the first partiton treated as FAT partiton"
+);
+
+U_BOOT_CMD(
+		iniload,    5,    0,    do_load_iniload,
+		"read and apply ini file from FAT partition",
+		"[<interface [<dev[:part]> [<filename> [<addr>]]]]\n"
+		"    - read filename from specified FAT partition. Defaults as iniload mmc 0:1 boot.ini CONFIG_SYS_LOAD_ADDR"
 );
 
 /* vim: set ts=4 sw=4 tw=80: */
